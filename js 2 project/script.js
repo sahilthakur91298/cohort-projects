c54a7ff5ff7b4a6fd299ebb5457f245b3d91ae91
Sahil Thakur <sahilthakur91298@gmail.com>
2025-11-18 10:41:33 +0530

    random ipl team

--- js 2 project/script.js
+++ js 2 project/script.js
@@ -1,11 +1,55 @@
-let box = document.querySelector(".box")
-let btn = document.querySelector("button")
+ let box = document.querySelector(".box");
+        let btn = document.querySelector("button");
+        let h1 = document.querySelector("#result");
+        let main = document.querySelector("body");
 
-btn.addEventListener("click",()=>{
-    let c1 = Math.floor(Math.random()*256);
-     let c2 = Math.floor(Math.random()*256);
-      let c3 = Math.floor(Math.random()*256);
+        let arr = [
+            {
+                team: "CSK",
+                winner: 2023,
+                primary: "#FDB913",
+                sec: "#00B8D4",
+            },
+            {
+                team: "RCB",
+                winner: 2025,
+                primary: "#EC1C24",
+                sec: "#1C1C1C",
+            },
+            {
+                team: "SRH",
+                winner: 2016,
+                primary: "#FF822A",
+                sec: "#000000",
+            },
+            {
+                team: "RR",
+                winner: 2008,
+                primary: "#254AA5",
+                sec: "#EA1A85",
+            },
+            {
+                team: "MI",
+                winner: 2020,
+                primary: "#004BA0",
+                sec: "#FFD700",
+            },
+        ];
 
-      box.style.backgroundColor = `rgb(${c1},${c2},${c3})`;
-      
-});
+
+        let winners = "";
+        arr.forEach((el, idx) => {
+            winners += `<h1 id="${idx}">${el.team}</h1>`;
+        });
+
+        box.innerHTML = winners;
+
+        btn.addEventListener("click", () => {
+            let randomIndex = Math.floor(Math.random() * arr.length);
+            let win = arr[randomIndex];
+            
+            h1.textContent = `${win.team} - Winner: ${win.winner}`;
+            h1.style.display = "block";
+            box.style.backgroundColor = win.sec;
+            main.style.backgroundColor = win.primary;
+        });
